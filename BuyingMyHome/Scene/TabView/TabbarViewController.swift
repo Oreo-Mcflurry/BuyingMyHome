0cfea6054e611291e0d36693677bbabfc23c4326
유인호 <yuiop1029@naver.com>
2024-03-10 17:05:39 +0900

    [ADD] add TabbarView

--- BuyingMyHome/Scene/TabView/TabbarViewController.swift
+++ BuyingMyHome/Scene/TabView/TabbarViewController.swift
@@ -11,18 +11,17 @@ class TabbarViewController: UITabBarController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		let homeVC = UINavigationController(rootViewController: ())
-		homeVC.tabBarItem = UITabBarItem(title: nil, image: .tabTrendInactive, selectedImage: .tabTrend)
+		let mapVC = UINavigationController(rootViewController: MapViewController())
 
-		let searchVC = UINavigationController(rootViewController: SearchViewController())
-		searchVC.tabBarItem = UITabBarItem(title: nil, image: .tabSearchInactive, selectedImage: .tabSearch)
+//		let searchVC = UINavigationController(rootViewController: SearchViewController())
+//		searchVC.tabBarItem = UITabBarItem(title: nil, image: .tabSearchInactive, selectedImage: .tabSearch)
+//
+//		let favoriteVC = UINavigationController(rootViewController: FavoriteViewController())
+//		favoriteVC.tabBarItem = UITabBarItem(title: nil, image: .tabPortfolioInactive, selectedImage: .tabPortfolio)
+//
+//		let profileVC = UINavigationController(rootViewController: ProfileViewController())
+//		profileVC.tabBarItem = UITabBarItem(title: nil, image: .tabUserInactive, selectedImage: .tabUser)
 
-		let favoriteVC = UINavigationController(rootViewController: FavoriteViewController())
-		favoriteVC.tabBarItem = UITabBarItem(title: nil, image: .tabPortfolioInactive, selectedImage: .tabPortfolio)
-
-		let profileVC = UINavigationController(rootViewController: ProfileViewController())
-		profileVC.tabBarItem = UITabBarItem(title: nil, image: .tabUserInactive, selectedImage: .tabUser)
-
-		self.viewControllers = [homeVC, searchVC, favoriteVC, profileVC]
+		self.viewControllers = [mapVC]
 	}
 }
