f026edde447a68f47f821bb047cc7ee8576cd876
유인호 <yuiop1029@naver.com>
2024-03-18 10:27:20 +0900

    [ADD] Firebase Analytics

--- BuyingMyHome/AppDelegate.swift
+++ BuyingMyHome/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import NMapsMap
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		NMFAuthManager.shared().clientId = APIkeys.naverMapClient.rawValue
+		FirebaseApp.configure()
 		return true
 	}
 
