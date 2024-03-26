a69c319195c2662eaa174de51dbe8fe47f08bf8b
유인호 <yuiop1029@naver.com>
2024-03-26 18:45:51 +0900

    [ADD] Add OnboardingView

--- BuyingMyHome/SceneDelegate.swift
+++ BuyingMyHome/SceneDelegate.swift
@@ -16,7 +16,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		guard let windowScene = (scene as? UIWindowScene) else { return }
 		window = UIWindow(frame: windowScene.coordinateSpace.bounds)
 		window?.windowScene = windowScene
-		window?.rootViewController = TabbarViewController() // RootViewController
+		if UserDefaults.standard[.isFirstRun] {
+			window?.rootViewController = TabbarViewController()
+		} else {
+			window?.rootViewController = OnboardingViewController()
+		}
+
 		window?.makeKeyAndVisible()
 	}
 
