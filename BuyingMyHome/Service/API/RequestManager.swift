165213a0f9c8a1300c2425795bebd7636c604858
유인호 <yuiop1029@naver.com>
2024-04-01 17:32:33 +0900

    [FIX] Fix Custom Session to Alamofire Session

--- BuyingMyHome/Service/API/RequestManager.swift
+++ BuyingMyHome/Service/API/RequestManager.swift
@@ -9,16 +9,16 @@ import Foundation
 import Alamofire
 
 final class RequestManager {
+
 	enum APIError: Error {
 		case error
 	}
 
 	func request<T: Decodable>(_ api: APIkind, _ type: T.Type, completionHandler: @escaping (T?, APIError?)->()) {
-		let configuratiuon = URLSessionConfiguration.default
-		configuratiuon.requestCachePolicy = .returnCacheDataElseLoad
-		let session = Session(configuration: configuratiuon)
-
-		session.request(api.url, parameters: api.parameter, headers: api.header).responseDecodable(of: T.self) { response in
+		AF.request(api.url, parameters: api.parameter, headers: api.header) { request in
+			request.cachePolicy = .returnCacheDataElseLoad
+		}.responseDecodable(of: T.self) { response in
+			debugPrint(response)
 			switch response.result {
 			case .success(let success):
 				completionHandler(success, nil)
